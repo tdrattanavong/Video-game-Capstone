70fd5bfc40c02cb8c8c78f966a815cff6742d29b
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-06-22 14:57:35 -0700

    Added methods in categories controller

---
+++ capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,104 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+import org.yearup.service.CategoryService;
+import org.yearup.service.ProductService;
+
+import java.util.List;
+
+// add the annotations to make this a REST controller
+// add the annotation to make this controller the endpoint for the following url
+// http://localhost:8080/categories
+// add annotation to allow cross site origin requests
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
+
+public class CategoriesController
+{
+    private CategoryService categoryService;
+    private ProductService productService;
+
+
+    // create an Autowired constructor to inject the categoryService and productService
+    @Autowired
+    public CategoriesController(CategoryService categoryService, ProductService productService)
+    {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+
+    // add the appropriate annotation for a get action
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll()
+    {
+        var categories = categoryService.getAllCategories();
+        if (categories.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        // find and return all categories
+        return ResponseEntity.ok(categories);
+    }
+
+    // add the appropriate annotation for a get action
+
+    @GetMapping("{id}")
+    public ResponseEntity<Category> getById(@PathVariable int id)
+    {
+        var category = categoryService.getById(id);
+        if(category == null) return ResponseEntity.notFound().build();
+        // get the category by id
+        return ResponseEntity.ok(category);
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return productService.listByCategoryId(categoryId);
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Category> addCategory(@RequestBody Category category)
+    {
+        var newCategory = categoryService.create(category);
+        // insert the category and return it with status 201 Created
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCategory);
+    }
+
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Category updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        // update the category by id and return the updated category (200 OK)
+        return categoryService.update(id, category);
+    }
+
+
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id)
+    {
+        // delete the category by id and return status 204 No Content
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
